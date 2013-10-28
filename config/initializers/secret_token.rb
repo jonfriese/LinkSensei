a70051df0f156b9516f2519a0cd4cd12c1799741
Brian Davis <contact@briandavis.name>
2013-10-28 16:14:34 -0700

    fixed containers edit view

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-JABProject::Application.config.secret_key_base = '211043ffc031b53a1d2deede26bfc98a8201507d751be291f1685d07483f6fd46132db8c4c3bf3f2ae91872084120d7f6aa30e67a6010339010cbbc26d50d33f'
+JABProject::Application.config.secret_key_base = ENV['RAILS_TOKEN']
