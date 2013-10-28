c658df0939476a3dd00b223247378cedf14387e9
Andre Bautista <andrebautista2513@gmail.com>
2013-10-28 15:44:06 -0700

    deleted scaffolded css file, and added font

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20131028185830) do
-
+ActiveRecord::Schema.define(version: 20131028211603) do
 
   create_table "containers", force: true do |t|
     t.integer  "user_id"
@@ -32,7 +30,6 @@ ActiveRecord::Schema.define(version: 20131028185830) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
