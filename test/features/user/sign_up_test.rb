10857a0c64a204ea9440822db9c20a0e471fa60a
Andre Bautista <andrebautista2513@gmail.com>
2013-10-29 14:37:47 -0700

    working on styling still

--- test/features/user/sign_up_test.rb
+++ test/features/user/sign_up_test.rb
@@ -1,18 +1,20 @@
 require "test_helper"
 
-class User::SignUpTest < Capybara::Rails::TestCase
-  test "user sign up" do
-
+feature "A user can sign in" do
+  scenario "user sign up" do
     visit root_path
-
     within("//div[@id='signup']") do
       fill_in "Enter email", with: "tester@example.com"
       fill_in "Password", with: "test1234"
       fill_in "Confirm password", with: "test1234"
     end
-
     click_on "Sign up"
-
     page.text.must_include "Welcome! You have signed up successfully."
   end
+
+  scenario "user tries to sign up with invalid data" do
+    click on "Sign up"
+
+  end
+
 end
