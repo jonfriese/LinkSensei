93d7cc4221a2115aee8bf1ec1752ef8b2c771481
noj33 <jonfriese33@gmail.com>
2013-10-30 13:48:48 -0700

    Twitter users arent linked to devise registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   def after_sign_in_path_for(resource)
     user_path(current_user)
