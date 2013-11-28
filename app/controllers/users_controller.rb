a47a79217b8ab619e0fe3aee44d9c7363ad4688f
Andre Bautista <andrebautista2513@gmail.com>
2013-11-27 19:46:39 -0800

    order of containers sorted fixes #22, modal window for edit link not 100% working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @containers = @user.containers.reject{|c| c.staging?}
+    @containers = @user.containers.order(created_at: :desc).reject{|c| c.staging?}
     @staging = @user.containers.select{|c| c.staging?}
     @link = Link.new
   end
