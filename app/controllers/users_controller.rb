a3a909e5aa690ec64769d8518cc17498ff99a533
Andre Bautista <andrebautista2513@gmail.com>
2013-11-28 00:03:10 -0800

    working on paginate but containers won't render properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @containers = @user.containers.order(created_at: :desc).reject{|c| c.staging?}
+    @containers = @containers.paginate(:page => params[:page], :per_page => 10)
     @staging = @user.containers.select{|c| c.staging?}
     @link = Link.new
   end
