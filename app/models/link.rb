ce948a3949726c3b016c19c4a17c2a925fefda57
noj33 <jonfriese33@gmail.com>
2013-11-30 21:28:03 -0800

    small model refactor

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,6 @@
 class Link < ActiveRecord::Base
   belongs_to :container
-  validates :url, presence: true
-  validates :url, uniqueness: true
+  validates :url, presence: true, uniqueness: true
   validates :name, presence: true
   validates :description, length: { in: 0..140 }
   validate :public_suffix
