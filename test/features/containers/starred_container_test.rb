8af2736066494f69374791e03e5f11b3b8731148
Brian Davis <contact@briandavis.name>
2013-12-02 10:59:36 -0800

    added test for starred containers

--- test/features/containers/starred_container_test.rb
+++ test/features/containers/starred_container_test.rb
@@ -3,26 +3,30 @@ require "test_helper"
 feature "A user can change a container starred settings" do
 
   scenario "User checks box on edit container page" do
-    visit root_path
-    within("//div[@id='signup']") do
-      fill_in "Full Name", with: "Test Guy"
-      fill_in "Enter email", with: "tester@example.com"
-      fill_in "Password", with: "test1234"
-      fill_in "Confirm password", with: "test1234"
-    end
-
-    click_on "Sign up"
-    page.text.must_include "Test Guy"
-
-    page.text.wont_include "★"
-    click_on "edit_container_2"
-    check "Starred"
-    click_on "Update Container"
-    page.text.must_include "★"
+    create_starred_container
   end
 
   scenario "Containers with stars are listed first" do
-    skip
+    # Given a starred container
+    create_starred_container
+
+    # and a newly created, non-starred container
+    page.find('#new_container_plus').click
+
+    fill_in "Name", with: "2nd container"
+    fill_in "Description", with: "A description"
+    click_on "Create Container"
+
+    # the starred container will preceed the unstarred container
+    page.text =~ /Start.*2nd/
+
+    # and when the unstarred container is starred,
+    click_on "edit_container_3"
+    check "Starred"
+    click_on "Update Container"
+
+    # it will preceed the previously starred container, as it was starred more recently.
+    page.text =~ /2nd.*Start/
   end
 end
 
