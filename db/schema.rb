8796da98ab624ff12360b96d0e76809d822b773d
noj33 <jonfriese33@gmail.com>
2013-12-03 12:05:56 -0800

    cloning view logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202223716) do
+ActiveRecord::Schema.define(version: 20131203193603) do
 
   create_table "containers", force: true do |t|
     t.integer  "user_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20131202223716) do
     t.string   "original_user_name"
     t.integer  "clone_count"
     t.integer  "parent_id"
+    t.integer  "original_user_id"
   end
 
   create_table "links", force: true do |t|
