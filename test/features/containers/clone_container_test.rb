9087d25122803c50868659d36a4adae220931f2f
noj33 <jonfriese33@gmail.com>
2013-12-03 14:12:46 -0800

    changed clone container test

--- test/features/containers/clone_container_test.rb
+++ test/features/containers/clone_container_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 feature "As a site user, I want to clone a container from another user, so that I can add interesting containers to my page" do
 
   #happy paths
-  scenario "User clones a container" do
+  scenario "User clones a container and user page is updated with container and link to original creator name" do
     sign_in
 
     create_container
@@ -18,7 +18,6 @@ feature "As a site user, I want to clone a container from another user, so that 
     sign_in
 
     click_on "Browse Users"
-
     click_on "Test Guy"
 
     # save_and_open_page
@@ -28,9 +27,15 @@ feature "As a site user, I want to clone a container from another user, so that 
 
     click_on "Home"
 
-    #check if on correct user page and that cloned container is present
-    page.text.must_include "Testy Tester"
+    #Check if user page has container and original author name
     page.has_content? "A container"
+    page.text.must_include "Created By: Test Guy"
 
+    click_on "Browse Users"
+    click_on "Test Guy"
+
+    #Check if page from whence the container was cloned has a clone count
+    page.text.must_include "Clone Count: 1"
+    page.text.wont_include "Created By: Test Guy"
   end
 end
