4922420f8ae6606e1e9e75bc4e553454a9725688
Andre Bautista <andrebautista2513@gmail.com>
2013-12-03 17:38:21 -0800

    Ah the javascript

---
+++ app/assets/javascripts/containers.js
@@ -0,0 +1,27 @@
+$(window).resize(function () {
+  if ($(window).width() > 919)  {
+    $(".exclude").removeAttr("style");
+    $("#reveal-this").removeAttr("style");
+    $(".exclude1").removeAttr("style")
+  }
+  else {
+    $(".exclude1").css("display", "none");
+  }
+});
+
+$(document).ready(function ()  {
+  $("#staging-link").on("click", function () {
+    $("#reveal-this").css('display', 'inline-table');
+    $("#staging-link").css('display', 'none');
+    $("#staging-link-2").css('display', 'inline');
+  });
+
+  $("#staging-link-2").on("click", function () {
+    $("#reveal-this").css('display', 'none');
+    $("#staging-link").css('display', 'inline');
+    $("#staging-link-2").css('display', 'none');
+  });
+});
+
+$('.info-pop').popover({trigger: 'hover'});
+
