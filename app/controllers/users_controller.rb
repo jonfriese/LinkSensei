e58e0affa0699fd831ce791b35f4df0fbaa6f197
noj33 <jonfriese33@gmail.com>
2013-12-04 00:02:51 -0800

    fixing mistake from last commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     unless @user.id == current_user.id || @user.public_status == true
       redirect_to "/", alert: "You aren't authorized to view that page!"
     end
-    @containers = current_user.containers.order(created_at: :desc).reject{|c| c.staging?}
+    @containers = @user.containers.order(created_at: :desc).reject{|c| c.staging?}
     @containers = @containers.paginate(:page => params[:page], :per_page => 10)
     @staging = current_user.containers.select{|c| c.staging?}
     @link = Link.new
